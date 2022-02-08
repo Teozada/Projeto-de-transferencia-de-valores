1e548ddebe1fbb8f9f1dc2e44b2f5a2762161572
Teozada <teodoro.contato@hotmail.com>
2022-02-08 13:59:49 -0300

    Fazendo o Model e configurando o docker

---
+++ src/main/java/com/provadigitalrepublic/ProvadigitalrepublicApplication.java
@@ -0,0 +1,13 @@
+package com.provadigitalrepublic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvadigitalrepublicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvadigitalrepublicApplication.class, args);
+	}
+
+}
